7692ea49ab30b65773dd2d6866bea2206b0a0a84
Marcin <marcin05092003@gmail.com>
2021-05-06 10:32:41 +0200

    no message

--- Frontend/beemanager/src/App.vue
+++ Frontend/beemanager/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <v-app>
-    <link
-      href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css"
-      rel="stylesheet"
-    />
     <NavigationDrawer
       :drawer = drawer
       @updateDrawer = 'updateDrawer'
